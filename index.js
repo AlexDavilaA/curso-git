6ecaaff65da6149871ba972de46dbd7233812dd6
Alejandro Davila <alejandrodavila1909610@gmail.com>
2023-03-13 04:30:00 -0600

    render post

--- index.js
+++ index.js
@@ -23,6 +23,20 @@
     }
 
    
+    const renderHtml = (user, post) => {
+        const content = document.getElementById('content');
+        content.innerHTML += `<h3>Post del usuario ${user.email}</h3>`;
+
+        post.forEach(post => {
+            content.innerHTML += `
+            <div class="post">
+            <h4>${post.title}</h4>
+            <p>${post.body}</p>
+            <br>
+            ${post.comments.map(c => `<p><span>${c.email}:</span>${c.body}</p>`).join('')}
+            `;
+        })
+    }
 
     const getBlogContent = async () => {
         try {
@@ -30,8 +44,7 @@
             const posts = await getPosts(user);
             await getCommentsForEachPost(posts);
 
-            console.log(user);
-            console.log(posts);
+            renderHtml(user, posts);
         } catch (err) {
             console.log(err);
         }
